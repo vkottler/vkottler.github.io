7e7de17b9048d7e0133f8b0e19fcd5a5894afd09
Vaughn Kottler <vaughnkottler@gmail.com>
2020-10-20 01:05:51 -0700

    structopt usage might be complete

--- svg-gen/src/main.rs
+++ svg-gen/src/main.rs
@@ -1,7 +1,29 @@
-use std::fs::File;
 use std::path::PathBuf;
-use std::io::Write;
+use std::str::FromStr;
 use structopt::StructOpt;
+
+#[derive(Debug)]
+enum FileType {
+    JSON,
+    TOML,
+    YAML,
+    SVG,
+}
+
+impl FromStr for FileType {
+    type Err = std::string::String;
+    fn from_str(file_type: &str) -> Result<Self, Self::Err> {
+        let mut str_type = String::from(file_type);
+        str_type.make_ascii_lowercase();
+        match str_type.as_str() {
+            "json" => Ok(FileType::JSON),
+            "svg" => Ok(FileType::SVG),
+            "toml" => Ok(FileType::TOML),
+            "yaml" => Ok(FileType::YAML),
+            _ => Err(format!("'{}' not a valid file type", file_type)),
+        }
+    }
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -9,21 +31,31 @@ use structopt::StructOpt;
     about = "An SVG (scalable vector graphic) file generator."
 )]
 struct Opt {
-    #[structopt(parse(from_os_str), default_value = "out.svg", help = "TODO")]
+    #[structopt(short, parse(from_os_str), help = "Path to the input manifest.")]
+    input: PathBuf,
+
+    #[structopt(long, help = "Input manifest type.")]
+    input_type: Option<FileType>,
+
+    #[structopt(long, default_value = "svg", help = "Output artifact type.")]
+    output_type: FileType,
+
+    #[structopt(
+        short,
+        parse(from_os_str),
+        default_value = "out.svg",
+        help = "Path to the output artifact."
+    )]
     output: PathBuf,
+
+    #[structopt(parse(from_os_str), help = "Additional include paths.")]
+    include_paths: Vec<PathBuf>,
 }
 
 fn main() -> std::io::Result<()> {
     let opt = Opt::from_args();
 
-    println!("{:?}", opt.output.extension());
+    println!("{:?}", opt);
 
-    // TODO, add svg extension if it's missing
-
-    let mut file = File::create(opt.output)?;
-
-    writeln!(file, "Hello, {}!", "world")?;
-
-    file.sync_all()?;
     Ok(())
 }
