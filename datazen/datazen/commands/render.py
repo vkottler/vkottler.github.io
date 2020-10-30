b41bc9111b8e9a6fb2bef86b1c389780aec6292d
Vaughn Kottler <vaughnkottler@gmail.com>
2020-10-30 02:26:54 -0700

    implement compile and render

---
+++ datazen/datazen/commands/render.py
@@ -0,0 +1,60 @@
+
+"""
+datazen - A command implementation for rendering configuration data (that has
+          optionally been resolved from variables) based on provided and
+          discovered templates.
+"""
+
+# built-in
+import logging
+from typing import List
+
+# third-party
+import jinja2
+
+# internal
+from datazen.parsing import update_dict_from_stream
+
+LOG = logging.getLogger(__name__)
+
+
+def get_template(template_dirs: List[str],
+                 template_name: str) -> jinja2.Template:
+    """
+    Load a jinja2 template from a list of directories where templates may
+    exist, and a name of a template to load.
+    """
+
+    # load specific template
+    return jinja2.Environment(
+            loader=jinja2.FileSystemLoader(template_dirs, followlinks=True),
+            trim_blocks=True,
+            lstrip_blocks=True
+    ).get_template(template_name)
+
+
+def str_render(template: jinja2.Template, config_data_path: str) -> str:
+    """ Load configuration data and render a jinja2 template with it. """
+
+    # load the configuration data from file
+    with open(config_data_path) as stream:
+        config_data = update_dict_from_stream(stream, config_data_path)
+
+    return template.render(config_data)
+
+
+def cmd_render(template_dirs: List[str], template_name: str,
+               config_data_path: str, output_file_path: str) -> bool:
+    """ Render the desired template from loaded configuration data. """
+
+    # load specific template
+    template = get_template(template_dirs, template_name)
+
+    # render the template to the output file, using the new data
+    with open(output_file_path, "w") as output:
+        output.write(str_render(template, config_data_path))
+
+    LOG.info("'%s' rendered from '%s' (template: '%s')", output_file_path,
+             config_data_path, template_name)
+
+    return True
--- datazen/datazen/parsing.py
+++ datazen/datazen/parsing.py
@@ -4,6 +4,7 @@ datazen - APIs for loading raw data from files.
 """
 
 # built-in
+import io
 import json
 import logging
 import os
@@ -11,12 +12,12 @@ from typing import TextIO
 
 # third-party
 import jinja2
-import yaml
+from ruamel import yaml
 
 LOG = logging.getLogger(__name__)
 
 
-def get_json_data(data_file: TextIO):
+def get_json_data(data_file: TextIO) -> dict:
     """ Load JSON data from a text stream. """
 
     data = None
@@ -27,15 +28,42 @@ def get_json_data(data_file: TextIO):
     return data
 
 
-def get_yaml_data(data_file: TextIO):
+def get_yaml_data(data_file: TextIO) -> dict:
     """ Load YAML data from a text stream. """
 
     data = None
     try:
-        data = yaml.full_load(data_file)
+        data = yaml.load(data_file)
     except (yaml.scanner.ScannerError, yaml.parser.ParserError) as exc:
         LOG.error("couldn't parse '%s' as yaml: %s", data_file.name, exc)
     return data
+
+
+def update_dict_from_stream(data_stream: TextIO, data_path: str,
+                            dict_to_update: dict = None) -> dict:
+    """
+    Load arbitrary data from a text stream, update an existing dictionary.
+    """
+
+    if not dict_to_update:
+        dict_to_update = {}
+
+    # get extension
+    ext = os.path.splitext(os.path.basename(data_path))[1][1:].lower()
+
+    # update the dictionary
+    if ext == "json":
+        dict_to_update.update(get_json_data(data_stream))
+    elif ext == "yaml":
+        dict_to_update.update(get_yaml_data(data_stream))
+    else:
+        LOG.error("can't load data from '%s' (unknown extension '%s')",
+                  data_path, ext)
+
+    if dict_to_update:
+        LOG.debug("loaded '%s' data from '%s'", ext, data_path)
+
+    return dict_to_update
 
 
 def load_and_resolve(data_path: str, variables: dict,
@@ -45,37 +73,12 @@ def load_and_resolve(data_path: str, variables: dict,
     the result as if it's a template using the provided variables.
     """
 
-    if not dict_to_update:
-        dict_to_update = {}
-
-    # get extension
-    ext = os.path.splitext(os.path.basename(data_path))[1][1:].lower()
-
+    # read the raw file and interpret it as a template, resolve 'variables'
     with open(data_path) as config_file:
+        template = jinja2.Template(config_file.read())
+        output = io.StringIO(template.render(variables))
 
-        # update the dictionary
-        if ext == "json":
-            dict_to_update.update(get_json_data(config_file))
-        elif ext == "yaml":
-            dict_to_update.update(get_yaml_data(config_file))
-        else:
-            LOG.error("can't load data from '%s' (unknown extension '%s')",
-                      data_path, ext)
-
-    if dict_to_update:
-
-        # attempt to resolve variables
-        jinja2.DictLoader(dict_to_update)
-        env = jinja2.Environment(loader=jinja2.DictLoader(dict_to_update),
-                                 trim_blocks=True, lstrip_blocks=True)
-        for key in dict_to_update:
-            template = env.get_template(key)
-            if key in variables:
-                dict_to_update[key] = template.render(variables[key])
-
-        LOG.debug("loaded '%s' data from '%s'", ext, data_path)
-
-    return dict_to_update
+    return update_dict_from_stream(output, data_path, dict_to_update)
 
 
 def load(data_path: str, dict_to_update: dict = None) -> dict:
