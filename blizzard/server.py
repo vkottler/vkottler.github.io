21812bc35a7080814c3fff12957e55af291bf8c2
Vaughn Kottler <vaughnkottler@gmail.com>
2021-05-11 08:53:26 +0000

    starting on server endpoint

---
+++ blizzard/server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+server - A simple API endpoint to support updating WoW macros.
+"""
+
+# built-in
+from datetime import datetime
+import logging
+import sys
+from typing import List
+import time
+
+# third-party
+from vtelem.classes.http_daemon import HttpDaemon
+from vtelem.classes.http_request_mapper import MapperAwareRequestHandler
+
+
+def main(_: List[str]) -> int:
+    """ Start a simple web server that runs the update routine. """
+
+    log = "{}.log".format(datetime.now().strftime("%d-%m-%Y-%H:%M:%S"))
+    logging.basicConfig(level=logging.INFO, filename=log)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    server = HttpDaemon("blizzard", ("0.0.0.0", 8000),
+                        MapperAwareRequestHandler)
+    try:
+        server.start()
+        while True:
+            time.sleep(2**32)
+    except KeyboardInterrupt:
+        server.stop()
+        server.close()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
