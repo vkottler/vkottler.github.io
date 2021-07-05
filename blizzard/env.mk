7a225fd27b70ed46a4d493131c6682c574319558
Vaughn Kottler <vaughnkottler@gmail.com>
2021-07-05 02:23:15 -0500

    sync with latest package things

--- blizzard/env.mk
+++ blizzard/env.mk
@@ -4,6 +4,8 @@ $($(PROJ)_DIR)/.secrets:
 	@rm -f $@
 	@echo "export BLIZZ_API_ID=`secrethub read vdoer/keybinds/blizzard/id`" >> $@
 	@echo "export BLIZZ_API_SECRET=`secrethub read vdoer/keybinds/blizzard/secret`" >> $@
+	@echo "export GITHUB_API_USER=vkottler" >> $@
+	@echo "export GITHUB_API_TOKEN=`secrethub read vkottler/github/tokens/public_repo-api-token`" >> $@
 	+@echo "wrote '$@'"
 
 check-env: | $($(PROJ)_DIR)/.secrets
@@ -12,4 +14,10 @@ ifndef BLIZZ_API_ID
 endif
 ifndef BLIZZ_API_SECRET
 	$(error BLIZZ_API_SECRET not set, run 'source $($(PROJ)_DIR)/.secrets')
+endif
+ifndef GITHUB_API_USER
+	$(error GITHUB_API_USER not set, run 'source $($(PROJ)_DIR)/.secrets')
+endif
+ifndef GITHUB_API_TOKEN
+	$(error GITHUB_API_TOKEN not set, run 'source $($(PROJ)_DIR)/.secrets')
 endif
--- blizzard/server.py
+++ blizzard/server.py
@@ -5,18 +5,27 @@ server - A simple API endpoint to support updating WoW macros.
 """
 
 # built-in
+from contextlib import contextmanager
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 import logging
+import os
+import subprocess
 import sys
 from threading import RLock
-from typing import List, Tuple
+from typing import List, Iterator, Optional, Tuple
 
 # third-party
+import git
+from datazen.classes.data_repository import DataRepository
 from vtelem.classes.http_daemon import HttpDaemon
 from vtelem.classes.http_request_mapper import MapperAwareRequestHandler
+import requests
 
 REPO_LOCK = RLock()
+REPO: Optional[DataRepository] = None
+REQUEST_NUM = 0
+REDIRECT = "https://vkottler.github.io/blizzard/static/add_macro.html"
 RESPONSE = """
 <!doctype html>
 <html>
@@ -30,20 +39,94 @@ RESPONSE = """
 """
 
 
-def add_macro_handler(_: BaseHTTPRequestHandler,
+@contextmanager
+def temporary_branch(repo: git.Repo, name: str = "temp",
+                     remote: str = "origin",
+                     main_branch: str = "master") -> Iterator[None]:
+    """
+    Given a repository object, attempt to switch to a new branch based on the
+    latest main branch from some origin (fetched first).
+
+    If the repsitory is in a dirty state, changes will be stashed and
+    unstashed.
+    """
+
+    return_branch = repo.active_branch
+    stashed = False
+    try:
+        # stash changes if we're in a dirty state
+        if repo.is_dirty() or repo.untracked_files:
+            repo.git.stash("push", "-u")
+            stashed = True
+
+        # fetch origin so we can put our temporary HEAD on the latest of the
+        # primary branch
+        repo.remotes[remote].fetch()
+        temp_branch = repo.create_head(name, "{}/{}".format(remote,
+                                                            main_branch))
+        temp_branch.checkout()
+        yield
+    finally:
+        return_branch.checkout()
+        if stashed:
+            repo.git.stash("apply")
+
+        # delete the temporary branch
+        repo.git.branch("-D", name)
+
+
+# pylint:disable=too-many-arguments
+def meld_and_push(repo: DataRepository, branch: str, data: dict, client: str,
+                  remote: str = "origin", main_branch: str = "master") -> str:
+    """
+    Given the repo object, a branch name and new data, meld it and commit it
+    and push the new branch to the remote.
+    """
+
+    with temporary_branch(repo.repo, branch, remote, main_branch):
+        with repo.meld(data, os.path.join("blizzard", "local", "macros")):
+            subprocess.run(["mk", "dz-sync"], check=True)
+            repo.repo.git.add(all=True)
+            repo.repo.git.commit("-m", "automated macro add")
+            repo.repo.git.push("-u", remote, branch)
+
+    # create pull request, get url of pull request
+    # https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
+    uri = "https://api.github.com/repos/{}/{}/pulls".format(
+        "vkottler", "vkottler.github.io"
+    )
+    title = "Form Submission ({})".format(client)
+    auth = (os.environ["GITHUB_API_USER"].strip(),
+            os.environ["GITHUB_API_TOKEN"].strip())
+    req = requests.post(
+        uri,
+        json={"head": branch, "base": main_branch, "title": title},
+        headers={
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": "token {}".format(auth[1]),
+        },
+        auth=auth,
+    )
+    if req.status_code == requests.codes["created"]:
+        return req.json()["html_url"]
+    print(req.text)
+    return REDIRECT
+
+
+# pylint:disable=global-statement
+def add_macro_handler(req: BaseHTTPRequestHandler,
                       data: dict) -> Tuple[bool, str]:
     """ Handle a request from 'add_macro.html' so we can add content. """
 
-    redirect = "https://vkottler.github.io/blizzard/static/add_macro.html"
+    global REQUEST_NUM
+    redirect = REDIRECT
+    assert REPO is not None
 
     # validate the request
     keys = ["class", "name", "icon", "description", "lines"]
     if all(key in data for key in keys):
-        redirect = "https://github.com/vkottler/vkottler.github.io/pulls"
-
         # make sure we don't handle concurrent requests
         with REPO_LOCK:
-
             # build the macro content
             macro = {
                 "name": data["name"][0].strip(),
@@ -51,23 +134,18 @@ def add_macro_handler(_: BaseHTTPRequestHandler,
                 "icon": data["icon"][0].strip(),
                 "lines": [x.strip() for x in data["lines"][0].split("\r\n")],
             }
-            print(macro)
+            klass = data["class"][0] if "class" in data else "generic"
+            spec = data["spec"][0] if "spec" in data else "generic"
 
-            # fetch the repository, pull main branch, create branch
-
-            # determine the destination, load it, add our entry, write back
-            print(data["class"][0])
-            if "spec" in data:
-                print(data["spec"][0])
-
-            # run 'mk dz-sync' on the checkout
-
-            # commit changes to branch, push
-
-            # create pull request, get url of pull request
-            # https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
-
-            # checkout main brainch
+            # on a new, temporary branch, meld the macro into existing data
+            temp_branch = "macro-{}-{}".format(
+                REQUEST_NUM,
+                datetime.now().strftime("%d-%m-%Y-%H_%M_%S"),
+            )
+            redirect = meld_and_push(REPO, temp_branch,
+                                     {klass: {spec: [macro]}},
+                                     req.client_address[0])
+            REQUEST_NUM += 1
 
     return True, RESPONSE.format(redirect).strip()
 
@@ -75,13 +153,18 @@ def add_macro_handler(_: BaseHTTPRequestHandler,
 def main(_: List[str]) -> int:
     """ Start a simple web server that runs the update routine. """
 
+    global REPO
     log = "{}.log".format(datetime.now().strftime("%d-%m-%Y-%H:%M:%S"))
-    logging.basicConfig(level=logging.INFO, filename=log)
+    logging.basicConfig(level=logging.DEBUG, filename=log)
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    assert "GITHUB_API_USER" in os.environ
+    assert "GITHUB_API_TOKEN" in os.environ
     server = HttpDaemon("blizzard", ("0.0.0.0", 8080),
                         MapperAwareRequestHandler)
     server.add_handler("POST", "add_macro", add_macro_handler,
                        response_type="text/html")
+    repo_loc = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+    REPO = DataRepository(repo_loc)
     return server.serve()
 
 
