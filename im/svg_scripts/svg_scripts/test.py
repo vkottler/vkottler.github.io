a475cac30b2144d9a4cf4f28e8eed83091132189
Vaughn Kottler <vaughnkottler@gmail.com>
2022-03-12 16:17:21 -0800

    Testing background etc

--- im/svg_scripts/svg_scripts/test.py
+++ im/svg_scripts/svg_scripts/test.py
@@ -5,10 +5,8 @@ svg_scripts - A test script for composing an SVG document.
 # built-in
 from typing import List
 
-# internal
-from svgen.attribute.style import Style
+# third-party
 from svgen.attribute.viewbox import ViewBox
-from svgen.color import Color
 from svgen.element import Element
 from svgen.element.circle import centered as circ_centered
 from svgen.element.rect import centered as rect_centered
@@ -17,14 +15,11 @@ from svgen.element.rect import centered as rect_centered
 def compose(viewbox: ViewBox, config: dict) -> List[Element]:
     """An example function for composing a document."""
 
-    result = []
+    result: List[Element] = []
 
     print(config)
 
-    red = Style().add_color(Color.from_ctor("red"))
-    blue = Style().add_color(Color.from_ctor("blue"))
-
-    result.append(circ_centered(viewbox).add_attribute(red))
-    result.append(rect_centered(viewbox, 0.5, 0.5).add_attribute(blue))
+    result.append(circ_centered(viewbox, 0.75, color="red"))
+    result.append(rect_centered(viewbox, 0.5, 0.5, color="blue"))
 
     return result
